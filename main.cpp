e655913a73be1b78330e6ae810515bcbbde4fb70
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-09 13:55:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+/*
+* Author: Dakota Hernandez
+* Assignment Title:
+* Assignment Description:
+* Due Date:
+* Date Created: 10/09/2024
+* Date Last Modified: 8/28/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    //data abstraction
+    //input
+    //process
+    //output
+    return 0;
+}
 
