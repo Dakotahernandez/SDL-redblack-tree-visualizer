3d0d019c63359b584c28110960e4110bb8de63e6
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-16 09:25:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 /*
-* Author: Dakota Hernandez
-* Assignment Title:
-* Assignment Description:
-* Due Date:
+* Authors:
+   Dakota Hernandez
+   Griffin Roe
+   Sofia Amador
+   Anjan Godavarti
+   Mason Baxter
+   Jonah Beck
+* Assignment Title:CSI 3334 Group project
+* Assignment Description:using SDL create a red-black tree visualizer using SDL
+* Due Date:12/10/2024
 * Date Created: 10/09/2024
-* Date Last Modified: 8/28/2024
+* Date Last Modified: 10/16/2024
 */
 
+//custom libraries
+#include "SDL_Plotter.h"
+#include "Constants.h"
+
+//C++ standard libaries
 #include <iostream>
+
+
 using namespace std;
 
 int main(){
     //data abstraction
+    
     //input
+    
     //process
+    
     //output
     return 0;
 }
+
 
