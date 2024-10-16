eea7d089cf6359c997b89d87547e2e66874b8e23
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-16 09:26:56 -0500

    adding file for constants

---
+++ Constants.h
@@ -0,0 +1,13 @@
+/*
+ This file will contain all constants
+ Comment what each constant is for
+ Constants are in all caps and use _ as space
+ example: CONSTANT_EXAMPLE
+ */
+#ifndef Constants_h
+#define Constants_h
+
+//size of screen displaying our redblack tree
+const int SCREEN_SIZE = 1000;
+
+#endif /* Constants_h */
