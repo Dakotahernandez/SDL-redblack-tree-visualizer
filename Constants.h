c691339952994dcbb5e909cf2e551ff5337a7a44
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-16 09:29:43 -0500

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -8,6 +8,13 @@
 #define Constants_h
 
 //size of screen displaying our redblack tree
-const int SCREEN_SIZE = 1000;
+const int SCREEN_SIZE                 = 1000;
 
+//color constants so there are consitant colors throughout project
+const color BROWN                      (165,42,42);
+const color RED                        (255,0,0);
+const color BLUE                       (0,0,255);
+const color GREEN                      (0,255,0);
+const color _WHITE                     (255,255,255);
+const color BLACK                      (0,0,0);
 #endif /* Constants_h */
