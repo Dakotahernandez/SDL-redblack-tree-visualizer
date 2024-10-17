705b37b817ec281d29fcc1172c74d9994f1712bb
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-17 14:44:54 -0500

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -1,4 +1,18 @@
 /*
+* Authors:
+   Dakota Hernandez
+   Griffin Roe
+   Sofia Amador
+   Anjan Godavarti
+   Mason Baxter
+   Jonah Beck
+* Assignment Title: CSI 3334 Group Project
+* Assignment Description: Using SDL, create a red-black tree visualizer using SDL
+* Due Date: 12/10/2024
+* Date Created: 10/09/2024
+* Date Last Modified: 10/16/2024
+*/
+/*
  This file will contain all constants
  Comment what each constant is for
  Constants are in all caps and use _ as space
