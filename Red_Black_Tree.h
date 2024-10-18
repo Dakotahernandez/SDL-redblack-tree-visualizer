423ea48a56fbc9722aca342f3516fd88f025c972
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-18 09:20:24 -0500

    Update Red_Black_Tree.h

--- Red_Black_Tree.h
+++ Red_Black_Tree.h
@@ -12,24 +12,79 @@
 * Date Created: 10/09/2024
 * Date Last Modified: 10/16/2024
 */
-#ifndef Red_Black_Tree_h
-#define Red_Black_Tree_h
+#ifndef RED_BLACK_TREE_H
+#define RED_BLACK_TREE_H
+
 #include <iostream>
 #include "Node.h"
 
 using namespace std;
+
 template <typename T>
-class Red_Black_Tree{
-    private:
-    Node<T>* Root;
-    
-    public:
-    void balance(){};
-    void insert(){};
-    void remove(){};
-    void erase(){};
-    void Right_rotate(){};
-    void Left_rotate(){};
+class Red_Black_Tree {
+private:
+    Node<T>* root;//Pointer to the root of the tree
+
+    //Helper functions
+    void balance(Node<T>* node); //Balances the tree after insertion or deletion
+    //rotations- these will be called by balance
+    void rightRotate(Node<T>* node);//Performs a right rotation on the node
+    void leftRotate(Node<T>* node); //Performs a left rotation on the node
+
+public:
+    // Constructors
+    Red_Black_Tree();//Default constructor
+    Red_Black_Tree(T rootValue);//Constructor with root initialization
+    Red_Black_Tree(const Red_Black_Tree& other);//Copy constructor
+    Red_Black_Tree(Red_Black_Tree&& other);//Move constructor
+
+    // Destructor
+    ~Red_Black_Tree();//Destructor to deallocate the tree
+
+    // Tree operations
+    void insert(T value);//Inserts a value into the tree
+    void remove(T value);//Removes a value from the tree
+    void clear();//Clears the entire tree
 };
 
-#endif /* Red_Black_Tree_h */
+//function defintions
+// Constructors
+template <typename T>
+Red_Black_Tree<T>::Red_Black_Tree(){
+    
+}
+template <typename T>
+Red_Black_Tree<T>::Red_Black_Tree(T rootValue){
+    
+}
+template <typename T>
+Red_Black_Tree<T>::Red_Black_Tree(const Red_Black_Tree& other){
+    
+}
+template <typename T>
+Red_Black_Tree<T>::Red_Black_Tree(Red_Black_Tree&& other){
+    
+}
+
+// Destructor
+template <typename T>
+Red_Black_Tree<T>::~Red_Black_Tree(){
+    
+}
+
+// Tree operations
+template <typename T>
+void Red_Black_Tree<T>::insert(T value){
+    
+}
+template <typename T>
+void Red_Black_Tree<T>::remove(T value){
+    
+}
+template <typename T>
+void Red_Black_Tree<T>::clear(){
+    
+}
+
+#endif /* RED_BLACK_TREE_H */
+
