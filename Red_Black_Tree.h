ff61385a65bdaf9f4d7258b12247008b2708c16a
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-18 09:24:25 -0500

    added function block comments

--- Red_Black_Tree.h
+++ Red_Black_Tree.h
@@ -12,6 +12,7 @@
 * Date Created: 10/09/2024
 * Date Last Modified: 10/16/2024
 */
+
 #ifndef RED_BLACK_TREE_H
 #define RED_BLACK_TREE_H
 
@@ -23,44 +24,115 @@ using namespace std;
 template <typename T>
 class Red_Black_Tree {
 private:
-    Node<T>* root;//Pointer to the root of the tree
+    Node<T>* root; //Pointer to the root of the tree
 
-    //Helper functions
-    void balance(Node<T>* node); //Balances the tree after insertion or deletion
-    //rotations- these will be called by balance
-    void rightRotate(Node<T>* node);//Performs a right rotation on the node
-    void leftRotate(Node<T>* node); //Performs a left rotation on the node
+    /*
+    * description: Balances the tree after insertion or deletion.
+    * return: None.
+    * precondition: A node has been inserted or deleted.
+    * postcondition: The tree is balanced according to red-black tree properties.
+    */
+    void balance(Node<T>* node);
+
+    /*
+    * description: Performs a right rotation on the node.
+    * return: None.
+    * precondition: A node exists that requires a right rotation.
+    * postcondition: The node has been right-rotated in the tree.
+    */
+    void rightRotate(Node<T>* node);
+
+    /*
+    * description: Performs a left rotation on the node.
+    * return: None.
+    * precondition: A node exists that requires a left rotation.
+    * postcondition: The node has been left-rotated in the tree.
+    */
+    void leftRotate(Node<T>* node);
 
 public:
-    // Constructors
-    Red_Black_Tree();//Default constructor
-    Red_Black_Tree(T rootValue);//Constructor with root initialization
-    Red_Black_Tree(const Red_Black_Tree& other);//Copy constructor
-    Red_Black_Tree(Red_Black_Tree&& other);//Move constructor
+    /*
+    * description: Default constructor for the red-black tree.
+    * return: None.
+    * precondition: None.
+    * postcondition: An empty red-black tree is created.
+    */
+    Red_Black_Tree();
 
-    // Destructor
-    ~Red_Black_Tree();//Destructor to deallocate the tree
+    /*
+    * description: Constructor with root initialization.
+    * return: None.
+    * precondition: A value for the root is provided.
+    * postcondition: A red-black tree is created with the specified root value.
+    */
+    Red_Black_Tree(T rootValue);
 
-    // Tree operations
-    void insert(T value);//Inserts a value into the tree
-    void remove(T value);//Removes a value from the tree
-    void clear();//Clears the entire tree
+    /*
+    * description: Copy constructor for the red-black tree.
+    * return: None.
+    * precondition: An existing red-black tree is provided.
+    * postcondition: A copy of the red-black tree is created.
+    */
+    Red_Black_Tree(const Red_Black_Tree& other);
+
+    /*
+    * description: Move constructor for the red-black tree.
+    * return: None.
+    * precondition: An rvalue reference to a red-black tree is provided.
+    * postcondition: The red-black tree is moved, leaving the original empty.
+    */
+    Red_Black_Tree(Red_Black_Tree&& other);
+
+    /*
+    * description: Destructor for the red-black tree.
+    * return: None.
+    * precondition: A red-black tree exists.
+    * postcondition: The red-black tree is deallocated and destroyed.
+    */
+    ~Red_Black_Tree();
+
+    /*
+    * description: Inserts a value into the red-black tree.
+    * return: None.
+    * precondition: A value is provided for insertion.
+    * postcondition: The value is inserted and the tree is balanced.
+    */
+    void insert(T value);
+
+    /*
+    * description: Removes a value from the red-black tree.
+    * return: None.
+    * precondition: A value exists in the tree for removal.
+    * postcondition: The value is removed and the tree is balanced.
+    */
+    void remove(T value);
+
+    /*
+    * description: Clears the entire red-black tree.
+    * return: None.
+    * precondition: The red-black tree exists.
+    * postcondition: The tree is cleared and all nodes are deallocated.
+    */
+    void clear();
 };
 
-//function defintions
+//function definitions
 // Constructors
 template <typename T>
 Red_Black_Tree<T>::Red_Black_Tree(){
     
 }
+
 template <typename T>
 Red_Black_Tree<T>::Red_Black_Tree(T rootValue){
     
 }
+
 template <typename T>
 Red_Black_Tree<T>::Red_Black_Tree(const Red_Black_Tree& other){
     
 }
+
 template <typename T>
 Red_Black_Tree<T>::Red_Black_Tree(Red_Black_Tree&& other){
     
@@ -77,10 +149,12 @@ template <typename T>
 void Red_Black_Tree<T>::insert(T value){
     
 }
+
 template <typename T>
 void Red_Black_Tree<T>::remove(T value){
     
 }
+
 template <typename T>
 void Red_Black_Tree<T>::clear(){
     
