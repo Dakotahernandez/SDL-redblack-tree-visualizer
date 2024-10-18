f2b5bc417d6f0ca78829361907beef1dfcc40b06
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-10-18 09:01:58 -0500

    Update Node.h

--- Node.h
+++ Node.h
@@ -12,26 +12,24 @@
 * Date Created: 10/09/2024
 * Date Last Modified: 10/16/2024
 */
-#ifndef Node_h
-#define Node_h
+#ifndef NODE_H
+#define NODE_H
+
 #include <iostream>
 using namespace std;
 
-
 template <typename T>
-struct Node{
-    //data
-    //left and right pointers for tree
-    Node<T>* left;
-    Node<T>* right;
-    //holds data in the node
-    T data;
-    //color is represented as a char R for red B for black
-    //in case of no color assigned us U for undecided
-    char color;
-    
-    
-    //functions
-    
+struct Node {
+    Node<T>* left;          // Pointer to left child node
+    Node<T>* right;         // Pointer to right child node
+    T data;                 // Data contained in the node
+    char color;             // 'R' for Red, 'B' for Black, 'U' for undecided
+
+    // Constructor with default arguments for left, right, and color
+    Node(T data, Node<T>* left = nullptr, Node<T>* right = nullptr, char color = 'U')
+        : data(data), left(left), right(right), color(color) {}
 };
-#endif /* Node_h */
+
+#endif /* NODE_H */
+
+
