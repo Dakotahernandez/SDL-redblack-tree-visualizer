fa4a877df5774297af740cebee09372aebe79f70
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2024-11-05 10:40:18 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,12 @@
    Griffin Roe
    Sofia Amador
    Anjan Godavarti
-   Mason Baxter
-   Jonah Beck
 * Assignment Title: CSI 3334 Group Project
 * Assignment Description: Using SDL, create a red-black tree visualizer using SDL
 * Due Date: 12/10/2024
 * Date Created: 10/09/2024
 * Date Last Modified: 10/16/2024
 */
-
 // Custom libraries
 #include "SDL_Plotter.h"
 #include "Constants.h"
