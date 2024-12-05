483ce067efdbac06c07f83bc70a161a2b41fa8cd
sofiaam1 <sofia_amador1@baylor.edu>
2024-12-05 12:02:43 -0600

    Update RedBlackTreeDisplayer.h

--- RedBlackTreeDisplayer.h
+++ RedBlackTreeDisplayer.h
@@ -9,14 +9,16 @@
 * Date Created: 10/09/2024
 * Date Last Modified: 10/16/2024
 */
-#ifndef REDBLACKTREEDISPLAYER_H
-#define REDBLACKTREEDISPLAYER_H
-
+#ifndef RedBlackTreeDisplayer_h
+#define RedBlackTreeDisplayer_h
 #include "SDL_Plotter.h"
 #include "Red_Black_Tree.h"
 #include "Constants.h"
 #include <cmath>
 
+struct nodeLocation {
+    
+};
 // Constants for color and spacing
 
 // Class to display a Red-Black Tree using SDL_Plotter
@@ -27,13 +29,13 @@ public:
         : plotter(plotter), width(width), height(height) {}
 
     // Main function to display the Red-Black Tree
-    void displayTree(Red_Black_Tree<int>& tree) {
+    void displayTree(Red_Black_Tree<T>& tree) {
         plotter.clear();
-        displayNode(tree.getRoot(), WINDOW_WIDTH / 2, TOP_MARGIN + NODE_RADIUS, WINDOW_WIDTH / 4, NODE_RADIUS);
+        displayNode(tree.getRoot(), width / 2, NODE_RADIUS * 2, width / 4, NODE_RADIUS);
         plotter.update();
     }
 
-private:
+//private:
     SDL_Plotter& plotter;
     int width, height;
 
@@ -55,7 +57,7 @@ private:
             }
         }
     }
-   
+    
     /*
      * description: draw line between two points
      * return: none
@@ -71,37 +73,32 @@ private:
         do{
             plotter.plotPixel(x1, y1, c.R, c.G, c.B);
             int e2 = err * 2;          //error adjustment/movement
-            if (e2 > -dy) {              
+            if (e2 > -dy) {            //horizontal direction
                 err -= dy;
                 x1 += sx;
             }
-            if (e2 < dx) {          
+            if (e2 < dx) {             //vertical direction
                 err += dx;
                 y1 += sy;
             }
         }while(x1 != x2 && y1 != y2);
     }
-   /*
+    
+    /*
      * description: draw rectangle with given width and height
      * return: none
      * precondition: xy coordinates valid for window dimensions
      * postcondition: filled rectangle is displayed at desired location
      */
-   void drawRectangle(int x, int y, int width, int height, color c){
+    void drawRectangle(int x, int y, int width, int height, color c){
         for(int i = 0; i < width; i++){
             for(int j = 0; j < height; j++) {
                 plotter.plotPixel(x + i, y + j, c.R, c.G, c.B);
             }
         }
     }
-
-    //Xiangyu added drawNumber in 11/20/2024
-    //^^^Moved your drawNumber functions to the cpp file
-    //moved to a cpp file because they got really long
-    //bit weird with templated class, can move back if doesnt work
-    //added template to bottom of cpp file so it runs, otherwise it wont
-
-   /*
+    
+    /*
      * description: increases pixel size from 1 pixel = 4 pixels
      * return: none
      * precondition: xy coordinates are scaled seperately if needed
@@ -115,29 +112,29 @@ private:
             }
         }
     }
-   /*
+    
+    /*
      * description: draws letter
      * return: none
      * precondition: letter is included in the selected group in func
      * postcondition: letter is drawn at desired location
      */
     void drawLetter(int x, int y, char letter, int scale, color c);
-   /*
+    /*
      * description: draws number
      * return: none
      * precondition: number is 4 digits or less
      * postcondition: number is drawn at desired location
      */
     void drawNumber(int x, int y, int value);
-
-   //feel free to move the buttons around to a diff location/change color/size
-   /*
+    
+    /*
      * description: draws the labeled remove button
      * return: none
      * precondition: means of drawing letters, scale is 2
      * postcondition: button is drawn at desired location
      */
-   void removeButton(){
+    void removeButton(){
         int x = 100;
         int y = 650;
         drawRectangle(x-28, y-30, 120, 60, CUTE_PINK);
@@ -148,8 +145,8 @@ private:
         drawLetter(x +49, y, 'v', 2, DARK_PINK);
         drawLetter(x +61, y, 'e', 2, DARK_PINK);
     }
-
-   /*
+    
+    /*
      * description: draws the labeled insert button
      * return: none
      * precondition: means of drawing letters, scale is 2
@@ -166,9 +163,8 @@ private:
         drawLetter(x +50, y, 'r', 2, DARK_PINK);
         drawLetter(x +60, y, 't', 2, DARK_PINK);
     }
-
-   //wanted to move title in top middle but need to move whole tree down
-   /*
+    
+    /*
      * description: draws the title "Red-Black Tree"
      * return: none
      * precondition: means of drawing letters, scale is 2
@@ -191,26 +187,26 @@ private:
         drawLetter(x+143, y, 'e', 2, N_GRAY);
         drawLetter(x+155, y, 'e', 2, N_GRAY);
     }
-
-   /*
+    
+    /*
      * description: draws a filled node circle with outline and num value
      * return: none
      * precondition: valid xy coordinates, value digits 4 or less
      * postcondition: node is drawn at desired location
      */
-   void drawNodeCircle(int x, int y, int radius, color OutC, color InC, int value) {
+    void drawNodeCircle(int x, int y, int radius, color OutC, color InC, int value) {
         drawCircle(x, y, radius + 2, OutC);     //outline
         drawCircle(x, y, radius, InC);          //inside
         drawNumber(x, y, value);
     }
-
-   /*
+    
+    /*
      * description: draws new node with value in top left corner before sort
      * return: none
      * precondition: value digits 4 or less
      * postcondition: node is drawn at inital location before placement
      */
-   void displayNewNode(const color nodeColor, int value){
+    void displayNewNode(const color nodeColor, int value){
         /*this is a green node in the top left that is created when a new node
          is created. during comparisons it should do the blue outline animation.
          it should be change color to black/red then glide to its new spot
@@ -219,8 +215,8 @@ private:
         int startY = 50;
         drawNodeCircle(startX, startY, NODE_RADIUS, N_GROU, nodeColor, value);
     }
-
-   /*
+    
+    /*
      * description: deletes a circle by drawing white
      * return: none
      * precondition: valid xy coordinates
@@ -229,11 +225,12 @@ private:
     void clearCircle(int x, int y, int radius) {
         drawCircle(x, y, radius, _WHITE);
     }
-
-
-   //basic function to move circle from one place to next
-   //NEED TO FIX: erases everything behind it! :(
-   void glideNode(int x1, int y1, int x2, int y2, color c){
+    
+    
+    
+    //ADDING NEW FUNC
+    
+    void glideNode(int x1, int y1, int x2, int y2, color c){
         int dx = x2 - x1;         //horizontal distance
         int dy = y2 - y1;         //vertical distance
         int steps = max(dx, dy);
@@ -253,24 +250,46 @@ private:
             plotter.Sleep(5);
         }
     }
-
-
-   /*
+    
+    /*
+    void glideNodeNUM(int x1, int y1, int x2, int y2, color OUTc, color INc, int value){
+        int dx = x2 - x1;         //horizontal distance
+        int dy = y2 - y1;         //vertical distance
+        int steps = max(dx, dy);
+        
+        int xRate = dx / steps;
+        int yRate = dy / steps;
+        int x = x1;
+        int y = y1;
+        for(int i = 0; i <= steps; i++){
+            drawNodeCircle(x, y, NODE_RADIUS, OUTc, INc, value);
+            plotter.update();
+            if(i < steps){
+                drawCircle(x, y, NODE_RADIUS, _WHITE);
+            }
+            x += xRate;
+            y += yRate;
+            plotter.Sleep(5);
+        }
+    }
+    */
+    
+    /*
      * description: indicates which node is being currently viewed
      * return: none
      * precondition: valid xy coordinates
      * postcondition: blue outline animation around node 2 times
      */
-   void pulsateOutline(int x, int y, color OC, color IC, int value) {
+    void pulsateOutline(int x, int y, color OC, color IC, int value) {
         //NODE_RADIUS == 20
-        for(int i = 0; i < 2; i++){                         //two cycles
-            for(int i = 0; i < 10; i++){                    //grows
+        for(int i = 0; i < 2; i++){     //two cycles
+            for(int i = 0; i < 10; i++){    //grows
                 plotter.update();
                 plotter.Sleep(20);
-                drawCircle(x, y, NODE_RADIUS + i, PO_BLUE); //outline
+                drawCircle(x, y, NODE_RADIUS + i, PO_BLUE);  //outline
                 drawNodeCircle(x, y, NODE_RADIUS, OC, IC, value);
             }
-            for(int i = 10; i > 0; i--){                    //shrinks
+            for(int i = 10; i > 0; i--){    //shrinks
                 plotter.update();
                 plotter.Sleep(20);
                 drawCircle(x, y, NODE_RADIUS + i, _WHITE);  //outline
@@ -278,14 +297,14 @@ private:
             }
         }
     }
-
-   /*
+    
+    /*
      * description: draws line with slow animation for arrows
      * return: none
      * precondition: valid xy coordinates
      * postcondition: line is incrementally drawn from one point to next
      */
-   void drawSlowLine(int x1, int y1, int x2, int y2, color c) {
+    void drawSlowLine(int x1, int y1, int x2, int y2, color c) {
         int dx = abs(x2 - x1);          //horizontal distance
         int dy = abs(y2 - y1);          //vertical distance
         int sx = (x1 < x2) ? 1 : -1;    //-1 move left, 1 move right
@@ -295,18 +314,18 @@ private:
             plotter.plotPixel(x1, y1, c.R, c.G, c.B);
             plotter.update();
             plotter.Sleep(1);
-            int e2 = err * 2;            //error adjustment/movement
-            if (e2 > -dy) {              //horizontal direction
+            int e2 = err * 2;               //error adjustment/movement
+            if (e2 > -dy) {                 //horizontal direction
                 err -= dy;
                 x1 += sx;
             }
-            if (e2 < dx) {               //vertical direction
+            if (e2 < dx) {                  //vertical direction
                 err += dx;
                 y1 += sy;
             }
         }while(x1 != x2 && y1 != y2);
     }
-   
+    
     /*
      * description: helper function to draw filled triangle using
      * a line between two points and a given y-coordinate
@@ -318,7 +337,7 @@ private:
         if (y1 == y0) return x0;
         return x0 + ((x1 - x0) * (y - y0)) / (y1 - y0);
     }
-   /*
+    /*
      * description: draws a filled triangle for arrowheads
      * return: none
      * precondition: three valid points xy coordinates of triangle
@@ -393,8 +412,7 @@ private:
         //this actually draws it using the three points
         drawFilledTriangle(Px1, Py1, Px2, Py2, Px3, Py3, c);
     }
-
-   /*
+    /*
      * description: draws an animated arrow
      * return: none
      * precondition: valid points xy coordinates
@@ -405,53 +423,59 @@ private:
         drawArrowhead(x1, y1, x2, y2, 17, c);
     }
     
-
-
+    
+    
     // Recursively displays each node in the tree
-   void displayNode(Node<T>* node, int x, int y, int horizontal_gap, int radius) {
+    void displayNode(Node<T>* node, int x, int y, int horizontal_gap, int radius) {
         titleName();
         removeButton();
         inputButton();
+        
+        glideNode(50, 50, 300, 300, CUTE_PINK);
+        
         if (node == nullptr) return;
 
         // Determine color based on node color
-        color nodeColor;
+        color nodeColor, nodeOutline;
         if (node->color == NODE_RED) {
+            nodeOutline = N_REDOU;
             nodeColor = RED;
         } else {
-            nodeColor = BLACK;
+            nodeOutline = BLACK;
+            nodeColor = N_GRAY;
         }
-
+        
         // Draw the current node
         drawNodeCircle(x, y, radius, nodeOutline, nodeColor, node->value);
-
-      //new stuffz
-      //new node in top left corner
-      displayNewNode(N_GREEN, node->value);
-      //blue line for green new node
+        
+        //SOFIA
+        displayNewNode(N_GREEN, node->value);
+        
+        //blue line for green new node
         pulsateOutline(50, 50, N_GROU, N_GREEN, node->value);
         
         //blue outline for node in tree
         pulsateOutline(x, y, nodeOutline, nodeColor, node->value);
-
-      
+        
+        
         // Draw and display the left child
         if (node->left != nullptr) {
             int childX = x - horizontal_gap;
             int childY = y + VERTICAL_GAP;
             drawArrow(x, y, childX, childY, LINE_COLOR);
-            displayNode(node->left, childX, childY, horizontal_gap / 2, radius);//Change in 11/20/2024: previous 0.8 *radius let each next node
-        }                                                                       //circle shrinks by 0.8.
+            displayNode(node->left, childX, childY, horizontal_gap / 2, radius);
+        }
 
         // Draw and display the right child
         if (node->right != nullptr) {
             int childX = x + horizontal_gap;
             int childY = y + VERTICAL_GAP;
             drawArrow(x, y, childX, childY, LINE_COLOR);
-            displayNode(node->right, childX, childY, horizontal_gap / 2, radius);//ditto
+            displayNode(node->right, childX, childY, horizontal_gap / 2, radius);
         }
     }
 };
 
-#endif // REDBLACKTREEDISPLAYER_H
+#endif /* RedBlackTreeDisplayer_h */
+
 
